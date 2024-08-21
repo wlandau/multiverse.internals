1599e5583aa7a0ae062b0d70b5952a7039d616e1
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-08-21 16:03:46 +0100

    use calendar month for staging period

--- R/staging_is_active.R
+++ R/staging_is_active.R
@@ -4,28 +4,24 @@
 #' @description Check if the stating universe is active.
 #' @return `TRUE` if the staging universe is active, `FALSE` otherwise.
 #' @param thresholds Character vector of `"%m-%d"` dates that the
-#'   staging universe becomes active.
-#' @param duration Positive integer, number of days that the staging
-#'   universe remains active after each threshold.
-#' @param today Character string with today's date in `"%Y-%m-%d"` format.
+#'   staging universe becomes active. Staging will then last for a full calendar
+#'   month until, but not including, the same date a month later.
+#' @param today Character string with today's date in `"%Y-%m-%d"` format or an
+#'   object convertible to POSIXlt format.
 #' @examples
 #' staging_is_active()
 staging_is_active <- function(
   thresholds = c("01-15", "04-15", "07-15", "10-15"),
-  duration = 30L,
-  today = format(Sys.Date(), "%Y-%m-%d")
+  today = Sys.Date()
 ) {
-  year <- format(as.Date(today), "%Y")
-  thresholds <- paste0(year, "-", thresholds)
-  spec <- "%Y-%m-%d"
-  spans <- lapply(
-    thresholds,
-    function(first) {
-      format(
-        seq(from = as.Date(first), by = "day", length.out = duration),
-        spec
-      )
-    }
-  )
-  today %in% unlist(spans)
+  today <- as.POSIXlt(today, tz = "UTC")
+  within_freeze <- function(x, today) {
+    mon <- today$mon + 1L
+    day <- today$mday
+    mon == x[1L] && day >= x[2L] || mon == x[1L] + 1L && day < x[2L]
+  }
+  thresholds <- strsplit(thresholds, split = "-", fixed = TRUE)
+  thresholds <- lapply(thresholds, as.integer)
+  within <- lapply(thresholds, within_freeze, today = today)
+  any(as.logical(within))
 }
