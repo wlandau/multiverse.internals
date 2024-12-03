bb6b0cffe93e8e54d07a8bda3b80f5711ef77d52
wlandau <will.landau.oss@gmail.com>
2024-12-03 09:04:24 -0500

    add a test

--- tests/testthat/test-update_topics.R
+++ tests/testthat/test-update_topics.R
@@ -4,6 +4,7 @@ test_that("update_topics()", {
   dir.create(path, recursive = TRUE)
   writeLines("bayesian description", file.path(path, "bayesian"))
   writeLines("hpc description", file.path(path, "hpc"))
+  writeLines("empty description", file.path(path, "empty"))
   meta <- data.frame(
     package = c("nope", "crew", "stantargets", "jagstargets"),
     title = c("x", "crew-title", "stantargets-title", "jagstargets-title"),
@@ -20,7 +21,17 @@ test_that("update_topics()", {
   )
   expect_equal(
     sort(list.files(path)),
-    sort(c("bayesian", "bayesian.html", "hpc", "hpc.html", "index.html"))
+    sort(
+      c(
+        "bayesian",
+        "bayesian.html",
+        "empty",
+        "empty.html",
+        "hpc",
+        "hpc.html",
+        "index.html"
+      )
+    )
   )
   out <- readLines(file.path(path, "index.html"))
   expect_true(any(grepl("bayesian.html", out, fixed = TRUE)))
@@ -35,4 +46,9 @@ test_that("update_topics()", {
   expect_false(any(grepl("crew", out, fixed = TRUE)))
   expect_true(any(grepl("stantargets", out, fixed = TRUE)))
   expect_true(any(grepl("jagstargets", out, fixed = TRUE)))
+  out <- readLines(file.path(path, "empty.html"))
+  expect_false(any(grepl("nope", out, fixed = TRUE)))
+  expect_false(any(grepl("crew", out, fixed = TRUE)))
+  expect_false(any(grepl("stantargets", out, fixed = TRUE)))
+  expect_false(any(grepl("jagstargets", out, fixed = TRUE)))
 })
