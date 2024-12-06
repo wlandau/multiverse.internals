bf3eef8c25583ede26f90c0ef4e3b782b26256a7
wlandau <will.landau.oss@gmail.com>
2024-12-06 15:23:46 -0500

    migrate a test

--- tests/testthat/test-meta_checks.R
+++ tests/testthat/test-meta_checks.R
@@ -2,13 +2,6 @@ test_that("meta_checks()", {
   out <- meta_checks(repo = "https://wlandau.r-universe.dev")
   expect_true(is.data.frame(out))
   expect_gt(nrow(out), 1L)
-  fields <- c(
-    "_status",
-    "_winbinary",
-    "_macbinary",
-    "_wasmbinary",
-    "_linuxdevel",
-    "_buildurl"
-  )
+  fields <- c("package", "build_url", "issues")
   expect_true(all(fields %in% colnames(out)))
 })
