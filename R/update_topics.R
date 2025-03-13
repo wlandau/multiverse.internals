b9d6a6046ffe4c852759932872f06805a3b9ae1a
wlandau <will.landau.oss@gmail.com>
2025-03-13 16:44:52 -0400

    Revamp topics website

--- R/update_topics.R
+++ R/update_topics.R
@@ -8,6 +8,12 @@
 #'   local file path to the topics repository source code.
 #' @param repo Character string, URL of the Community universe.
 #' @param mock List of named objects for testing purposes only.
+#' @examples
+#' \dontrun{
+#' path <- tempfile()
+#' gert::git_clone("https://github.com/r-multiverse/topics", path = path)
+#' update_topics(path = path, repo = "https://community.r-multiverse.org")
+#' }
 update_topics <- function(
   path,
   repo = "https://community.r-multiverse.org",
@@ -16,65 +22,31 @@ update_topics <- function(
   meta <- mock$meta %||% meta_packages(repo, fields = c("Title", "URL"))
   meta <- meta[, c("package", "title", "url")]
   unlink(file.path(path, "*.html"))
-  topics <- setdiff(
-    list.files(path),
-    c(".gitignore", "README.md", "LICENSE.md")
-  )
+  topics <- list.files(file.path(path, "topics"))
   for (topic in topics) {
     update_topic(topic, path, meta)
   }
   topic_urls <- file.path(
-    "https://r-multiverse.org/topics", paste0(topics, ".html")
+    "https://r-multiverse.org/topics",
+    paste0(topics, ".html")
   )
-  topic_list <- paste0(
-    "<li>",
-    topics,
-    ": <a href=\"",
-    topic_urls,
-    "\">",
-    topic_urls,
-    "</a>",
-    "</li>"
-  )
-  topic_text <- paste(topic_list, collapse = "\n")
-  template <- system.file(
-    file.path("topics", "index.html"),
-    package = "multiverse.internals",
-    mustWork = TRUE
-  )
-  text <- readLines(template)
-  text <- gsub(pattern = "TOPICS", replacement = topic_text, x = text)
-  writeLines(text, file.path(path, "index.html"))
+  topic_list <- sprintf("* [%s](%s)", topics, topic_urls)
+  template <- readLines(file.path(path, "index.md"))
+  text <- c(template, "", topic_list)
+  writeLines(text, file.path(path, "index.md"))
 }
 
 update_topic <- function(topic, path, meta) {
   url <- file.path("https://r-multiverse.org/topics", paste0(topic, ".html"))
-  meta <- meta[grepl(pattern = url, x = meta$url, fixed = TRUE), ]
-  about <- readLines(file.path(path, topic))
-  template <- system.file(
-    file.path("topics", "topic.html"),
-    package = "multiverse.internals",
-    mustWork = TRUE
-  )
-  line <- paste0(
-    "<li>",
-    "<a href=\"https://community.r-multiverse.org/PACKAGE\">PACKAGE</a>: ",
-    "TITLE",
-    "</li>"
-  )
-  packages <- character(0L)
-  for (row in seq_len(nrow(meta))) {
-    package <- meta$package[row]
-    title <- meta$title[row]
-    element <- line
-    element <- gsub(pattern = "PACKAGE", replacement = package, x = element)
-    element <- gsub(pattern = "TITLE", replacement = title, x = element)
-    packages <- c(packages, element)
-  }
+  meta <- meta[grepl(pattern = url, x = meta$url, fixed = TRUE),, drop = FALSE] # nolint
+  about <- readLines(file.path(path, "topics", topic))
+  urls <- file.path("https://community.r-multiverse.org", meta$package)
+  packages <- sprintf("|[%s](%s)|%s|", meta$package, urls, meta$title)
   packages <- paste(packages, collapse = "\n")
+  template <- file.path(path, "topic.md")
   text <- readLines(template)
   text <- gsub(pattern = "ABOUT", replacement = about, x = text)
   text <- gsub(pattern = "PACKAGES", replacement = packages, x = text)
   text <- gsub(pattern = "TOPIC", replacement = topic, x = text)
-  writeLines(text, file.path(path, paste0(topic, ".html")))
+  writeLines(text, file.path(path, paste0(topic, ".md")))
 }
--- tests/testthat/test-update_topics.R
+++ tests/testthat/test-update_topics.R
@@ -1,10 +1,23 @@
 test_that("update_topics()", {
   path <- tempfile()
   on.exit(unlink(path))
-  dir.create(path, recursive = TRUE)
-  writeLines("bayesian description", file.path(path, "bayesian"))
-  writeLines("hpc description", file.path(path, "hpc"))
-  writeLines("empty description", file.path(path, "empty"))
+  dir.create(path)
+  dir.create(file.path(path, "topics"))
+  skip_if_offline()
+  for (file in c("index.md", "topic.md")) {
+    download.file(
+      url = file.path(
+        "https://raw.githubusercontent.com/r-multiverse",
+        "topics/refs/heads/main",
+        file
+      ),
+      destfile = file.path(path, file),
+      quiet = TRUE
+    )
+  }
+  writeLines("bayesian description", file.path(path, "topics", "bayesian"))
+  writeLines("hpc description", file.path(path, "topics", "hpc"))
+  writeLines("empty description", file.path(path, "topics", "empty"))
   meta <- data.frame(
     package = c("nope", "crew", "stantargets", "jagstargets"),
     title = c("x", "crew-title", "stantargets-title", "jagstargets-title"),
@@ -20,33 +33,32 @@ test_that("update_topics()", {
     mock = list(meta = meta)
   )
   expect_equal(
-    sort(list.files(path)),
+    sort(list.files(path, pattern = "\\.md$")),
     sort(
       c(
-        "bayesian",
-        "bayesian.html",
-        "empty",
-        "empty.html",
-        "hpc",
-        "hpc.html",
-        "index.html"
+        "bayesian.md",
+        "empty.md",
+        "hpc.md",
+        "index.md",
+        "topic.md"
       )
     )
   )
-  out <- readLines(file.path(path, "index.html"))
+  out <- readLines(file.path(path, "index.md"))
   expect_true(any(grepl("bayesian.html", out, fixed = TRUE)))
+  expect_true(any(grepl("empty.html", out, fixed = TRUE)))
   expect_true(any(grepl("hpc.html", out, fixed = TRUE)))
-  out <- readLines(file.path(path, "hpc.html"))
+  out <- readLines(file.path(path, "hpc.md"))
   expect_false(any(grepl("nope", out, fixed = TRUE)))
   expect_true(any(grepl("crew", out, fixed = TRUE)))
   expect_false(any(grepl("stantargets", out, fixed = TRUE)))
   expect_false(any(grepl("jagstargets", out, fixed = TRUE)))
-  out <- readLines(file.path(path, "bayesian.html"))
+  out <- readLines(file.path(path, "bayesian.md"))
   expect_false(any(grepl("nope", out, fixed = TRUE)))
   expect_false(any(grepl("crew", out, fixed = TRUE)))
   expect_true(any(grepl("stantargets", out, fixed = TRUE)))
   expect_true(any(grepl("jagstargets", out, fixed = TRUE)))
-  out <- readLines(file.path(path, "empty.html"))
+  out <- readLines(file.path(path, "empty.md"))
   expect_false(any(grepl("nope", out, fixed = TRUE)))
   expect_false(any(grepl("crew", out, fixed = TRUE)))
   expect_false(any(grepl("stantargets", out, fixed = TRUE)))
