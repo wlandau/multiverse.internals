0bc5fce7933843c25d24eb9bf7acc857d3b8f610
wlandau <will.landau.oss@gmail.com>
2025-03-15 16:08:46 -0400

    lint

--- tests/testthat/test-status_descriptions.R
+++ tests/testthat/test-status_descriptions.R
@@ -2,7 +2,7 @@ test_that("status_descriptions() mocked", {
   status <- status_descriptions(meta = mock_meta_packages)
   expected <- list(
     asylum = list(cran = "1.1.2"),
-    audio.whisper = list(remotes = "bnosac/audio.vadwebrtc"), 
+    audio.whisper = list(remotes = "bnosac/audio.vadwebrtc"),
     IMD = list(cran = "1.2.2"),
     stantargets = list(
       remotes = c("hyunjimoon/SBC", "stan-dev/cmdstanr")
