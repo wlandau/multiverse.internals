11faa38321d25336d170b6251e6b45844adcb3ae
wlandau <will.landau.oss@gmail.com>
2025-03-16 00:01:24 -0400

    lints and spelling

--- R/record_status.R
+++ R/record_status.R
@@ -69,7 +69,7 @@ record_status <- function(
   issues <- names(Filter(\(x) !x$success, status))
   status <- add_issues(status, issues_dependencies(issues, meta, verbose))
   status <- status[order(names(status))]
-  jsonlite::write_json(x = status, path = output, pretty = TRUE) 
+  jsonlite::write_json(x = status, path = output, pretty = TRUE)
   invisible()
 }
 
